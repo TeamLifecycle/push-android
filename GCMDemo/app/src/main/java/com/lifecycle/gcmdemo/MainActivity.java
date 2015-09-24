21d920685f5f6e9c294f5ae28bb894da85fe0861
Joseph Landon <smooth@Josephs-MacBook-Pro.local>
2015-09-24 13:54:45 -0500

    demo project for android push with gcm

---
+++ GCMDemo/app/src/main/java/com/lifecycle/gcmdemo/MainActivity.java
@@ -0,0 +1,91 @@
+package com.lifecycle.gcmdemo;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    String GCM_PROJECT = "1039285805057";
+    String GCM_KEY = "AIzaSyDJxgrvsUsuX6Fpt8Cg3sQ9GczCDd9VByQ";
+
+    Button btnRegId;
+    EditText etRegId;
+    GoogleCloudMessaging gcm;
+    String regid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnRegId = (Button) findViewById(R.id.btnGetRegId);
+        etRegId = (EditText) findViewById(R.id.etRegId);
+
+        btnRegId.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getRegId(){
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
+                    }
+                    regid = gcm.register(GCM_PROJECT);
+                    msg = "Device registered, registration ID=" + regid;
+                    Log.i("GCM", msg);
+
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                etRegId.setText(msg + "\n");
+            }
+        }.execute(null, null, null);
+    }
+
+    @Override
+    public void onClick(View v) {
+        getRegId();
+    }
+}
